140b2cce421edcf1c41a66b92ab2f0b019dd9273
pontat <ponta.t.dev@gmail.com>
2021-02-23 23:44:37 +0900

    feat:vite axios実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import axios from 'axios'
 import HelloWorld from './components/HelloWorld.vue'
+import { baseUrl } from './util/Util'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  mounted: async function () {
+    await axios.get(`${baseUrl}/health_check`)
+  },
 }
 </script>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,19 +1,21 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      count: 0
+      count: 0,
     }
-  }
+  },
 }
 </script>
